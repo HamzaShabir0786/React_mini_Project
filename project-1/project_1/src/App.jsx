76db691a520fb401050332137737126aa96259e1
Hamza Shabir <hamzii2205@gmail.com>
2024-11-29 21:56:55 +0500

    project 1 has completed

--- project-1/project_1/src/App.jsx
+++ project-1/project_1/src/App.jsx
@@ -1,40 +1,90 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect, useRef } from "react";
 import "./App.css";
-import { stringify } from "postcss";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+  // useRef hook
+  const passwordRef = useRef(null);
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (numberAllowed) str += "013456789";
     if (charAllowed) str += "@#$%^&*()_+?/{}[]~";
-    for (let i = 1; i <= Array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+  
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 100);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
   return (
     <>
-      <div className="ml-[22rem] w-[50%] rounded-lg px-12 p-4 my-8 text-orange-500 text-center font-bold text-[1rem] bg-gray-800">
-        password generator
-        <div className="flex shadow rounded-lg overflow-hidden h-[2rem] bg-purple-600">
+      <div className="ml-[30%] w-[40%]  rounded-lg px-12 p-4 pb-8 my-8 text-orange-500 text-center font-bold text-[1rem] bg-gray-800 ">
+        Password Generator
+        <div className="flex shadow rounded-lg overflow-hidden h-[2rem] bg-purple-600 mt-4">
           <input
             type="text"
             value={password}
-            className="outline-none w-full py-1 px3"
+            className="outline-none w-full py-1 px-3 "
             placeholder="password"
             readOnly
-          />{" "}
-          <button className="bg-blue-600 text-white p-1">Copy</button>
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className="bg-blue-600 text-white p-1 focus:bg-orange-500"
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2 mt-4">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={8}
+              max={40}
+              value={length}
+              className="cursor-pointer "
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label> Length: {length} </label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="character"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="character">Characters</label>
+          </div>
         </div>
       </div>
     </>
   );
 }
-
 export default App;
